929cd5c5d015f816119e9a9f7ad09fec3a01f5e2
stavchan <stavchan@hotmail.com>
2016-05-28 22:30:34 +0300

    Update db_conx.php

--- scripts/db_conx.php
+++ scripts/db_conx.php
@@ -5,8 +5,11 @@
 	$database = 'digitallyjukebox';
 	
 	$conx = mysqli_connect($host, $user, $pass, $database);
-
+	
+	//Επιστροφή αριθμού σφάλματος mysql efoson vrethei
 	if(mysqli_connect_errno()) die("Database connecton failed: " . mysqli_connect_error()." ".mysqli_connect_errno());
+	
+	//Dhlwsh kwdikopo;ihshs xarakthrwn vashs dedomenwn
 	mysqli_query($conx, "SET NAMES 'utf8'");
 	mysqli_query($conx, "SET CHARACTER SET 'utf8'");
 ?>
